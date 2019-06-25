67f17dadb1a2d30ff84925ade62d180a2a4922cd
arq-gabo <jgabrielguerral@gmail.com>
2019-06-25 23:48:24 +0000

    My Firts Commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+
+/**
+ * times_table - Function that prints the 9 times table, starting with 0
+ *
+ * Return: Always void
+ */
+#include "holberton.h"
+
+/**
+ * times_table - Function that prints the 9 times table, starting with 0
+ *
+ * Return: Always void
+ */
+void times_table(void)
+{
+	int a, b, c, i, j, x;
+
+	for (a = 0 ; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			x = (a == 3 && c == 9) || (a > 4 && b == 1);
+
+			if (c < 10)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				i = c / 10;
+				j = c % 10;
+				_putchar(i + '0');
+				_putchar(j + '0');
+			}
+			if (b == 9)
+			{
+				_putchar(10);
+			}
+			else if (c >= 10 || ((a == 2 || a == 4) && c == 8) || x)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+}
