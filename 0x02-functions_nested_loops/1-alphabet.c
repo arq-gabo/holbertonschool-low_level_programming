2b68a963be064b80701a641a97941234581f2bb7
arq-gabo <jgabrielguerral@gmail.com>
2019-06-25 17:13:23 +0000

    My Firts Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+  
+  int m;
+
+  for( m = 'a' ; m <= 'z' ; m++)
+    {
+      _putchar(m);
+    }
+
+
+  _putchar(10);
+
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+void print_alphabet_x10(void)
+
+{
+
+
+  int m, n;
+  n = 1;
+
+  while( n <= 10 )
+    {
+
+  for( m = 'a' ; m <= 'z' ; m++)
+    {
+      _putchar(m);
+    }
+
+  n++;
+  _putchar(10);
+
+    }
+
+
+}
