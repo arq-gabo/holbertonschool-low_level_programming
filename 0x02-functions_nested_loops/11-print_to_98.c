3390e9c5db45b4f28c41280f6e7467be981e7082
arq-gabo <jgabrielguerral@gmail.com>
2019-06-26 17:40:03 +0000

    My Firts Commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Function that prints natural numbers from n to 98
+ *
+ * @n: The number to be checked
+ */
+
+void print_to_98(int n)
+{
+
+	if (n < 98)
+	{
+		for ( ; n < 98 ; n++)
+			{
+		printf("%d, ", n);
+			}
+	}
+
+	else if (n > 98)
+	{
+		for ( ; n > 98 ; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+
+	else
+	{
+		printf("%d, ", n);
+	}
+
+}
