457c87b8be93accee87cbb4741644a876ff31e6e
arq-gabo <jgabrielguerral@gmail.com>
2019-06-27 22:24:13 +0000

    My Firts Commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,15 @@
 #include "holberton.h"
-
 /**
  * print_line - Function that draws a straight line in the terminal
  * @n: number of times of _
  */
-
 void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a <= n ; a++)
+	for (a = 0; a < n ; a++)
 	{
-		if (n > 0)
+		if (n >= 0)
 		{
 		_putchar('_');
 		}
