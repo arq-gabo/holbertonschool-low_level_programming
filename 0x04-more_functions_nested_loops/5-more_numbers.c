05fa7c90749aa62e9157961e310dc0d69548e3ea
arq-gabo <jgabrielguerral@gmail.com>
2019-06-27 21:56:34 +0000

    My Firts Commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Function that prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+
+	int a, b, c;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 0 ; b <= 14 ; b++)
+		{
+			if (b >= 0 && b <= 9)
+			{
+				c = ( b + '0');
+			}
+			else
+			{
+				c = ((b % 10) + '0');
+				_putchar(b / 10 + '0');
+			}
+			
+			_putchar(c);
+		}
+		_putchar(10);
+
+	}
+
+}
