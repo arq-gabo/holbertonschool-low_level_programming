66c9f3cbdbb059f637c44fe09b71f6949565ea95
arq-gabo <jgabrielguerral@gmail.com>
2019-06-27 20:11:01 +0000

    My Firts Commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
-* _isupper - Function that checks for uppercase character
-* @c: Number tu be checked
-* Return: 1 if c is uppercase and 0 otherwise 
+*_isupper - Function that checks for uppercase character
+*@c: Number tu be checked
+*Return: 1 if c is uppercase and 0 otherwise
 */
 
 int _isupper(int c)
