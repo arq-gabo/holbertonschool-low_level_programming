7fc28fe3692ef3ed0344d45fe689a13d38f8fb5e
arq-gabo <jgabrielguerral@gmail.com>
2019-06-28 00:53:31 +0000

    My Firts Commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int a;
+
+	for (a = 1; a <= 100 ; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0 && a % 3 != 0)
+		{
+			if (a <= 99)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
