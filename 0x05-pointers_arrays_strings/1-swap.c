a866689c013f8272f06a3e2e89ed7de263902b73
arq-gabo <jgabrielguerral@gmail.com>
2019-07-02 19:02:28 +0000

    Pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,16 +3,19 @@
 
 /**
  * main - check the code for Holberton School students.
- *
+ *@a:first pinter variable
+ *@b:second pointer variable
  * Return: Always 0.
  */
+
 void swap_int(int *a, int *b)
+
 {
 
-	int tmp;
+	int c;
 
-	tmp = *b;
+	c = *b;
 	*b = *a;
-	*a = tmp;
+	*a = c;
 
 }
