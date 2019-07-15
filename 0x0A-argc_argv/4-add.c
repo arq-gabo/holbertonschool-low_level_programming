eacfc132496529f2472ba82f5bcf741119a12374
arq-gabo <jgabrielguerral@gmail.com>
2019-07-15 04:51:38 +0000

    Argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <holberton.h>
+
+/**
+ * main - entry point
+ * @argc: contains the number of arguments that have been entered
+ * @argv: array of pointers to characters
+ * Return: Success or failure
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int i, j, k = 0;
+
+	unsigned int count = argc;
+
+	for (i = 1; i < count; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		k += atoi(argv[i]);
+	}
+	printf("%u\n", k);
+	return (0);
+}
