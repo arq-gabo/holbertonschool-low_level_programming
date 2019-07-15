9287c5098a09e77797005e0484f7fbf055c4e5b1
arq-gabo <jgabrielguerral@gmail.com>
2019-07-15 02:39:20 +0000

    Argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include"holberton.h"
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: contains the number of arguments that have been entered
+ * @argv: array of pointers to characters
+ * Return: Success or failure
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+
+}
