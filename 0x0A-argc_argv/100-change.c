5f03d14b56b19b0ec0af893df0b19553082919bd
arq-gabo <jgabrielguerral@gmail.com>
2019-07-15 21:54:17 +0000

    Argument

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,6 @@
-#include <holberton.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define LEN 5
 
 /**
  * main - Entry Point
@@ -7,13 +9,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int i, x, count = 0;
 
-	int y[LEN] = 1, 2, 5, 10, 25;
+	int y[LEN] = {1, 2, 5, 10, 25};
 
-	if (agrc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
