4046c3971cd1026e2dfc17bdc40bdc546bbfcf06
arq-gabo <jgabrielguerral@gmail.com>
2019-07-15 03:10:38 +0000

    Arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 
 /**
- *
- *
- *
- *
+ * main - entry point
+ * @argc: contains the number of arguments that have been entered
+ * @argv: array of pointers to characters
+ * Return: Success or failure
  */
 
 int main(int argc, char *argv[])
