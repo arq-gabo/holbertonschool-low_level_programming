313c8043e450222b1a9faaea18118df390f6dc0c
arq-gabo <jgabrielguerral@gmail.com>
2019-07-17 04:42:14 +0000

    Malloc_free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free memory of a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+
+	int count;
+
+	if (!grid)
+	{
+		free(grid);
+	}
+	for (count = 0; count < height; count++)
+	{
+		free(grid[count]);
+	}
+	free(grid);
+
+}
