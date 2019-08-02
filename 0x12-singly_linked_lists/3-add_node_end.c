b8316863bca220be53fa05f397dd0bbc7565b14b
arq-gabo <jgabrielguerral@gmail.com>
2019-08-02 21:27:14 +0000

    [FIX]Modify_parameters

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,28 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "lists.h"
-
 /**
- * add_node_end - adds a new node at the end of a list.
- * @head: a pointer
- * @str: a constant string
- * Return: A list.
+ *add_node_end - adds a new node at the end of a list_t list.
+ *@str: string to add in new nodo
+ *@head: data in list_t.
+ *Return: return a pointer to new nodo.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	size_t length = 0;
+	list_t *nodo;
+	list_t *puntero;
+	int cont;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	for (cont = 0; str[cont] != '\0'; cont++)
+	{
+	}
+
+	nodo = malloc(sizeof(list_t));
+
+	if (!nodo)
 		return (NULL);
 
-	new->str = strdup(str);
-
-	while (str[length])
-		length++;
-	new->len = length;
-	new->next = *head;
-	*head = new;
-
+	nodo->str = strdup(str);
+	nodo->len = cont;
+	nodo->next = NULL;
+	if (*head == NULL)
+	{
+		*head = nodo;
+	}
+	else
+	{
+		puntero = *head;
+		while (puntero->next)
+		{
+			puntero = puntero->next;
+		}
+		puntero->next = nodo;
+	}
 	return (*head);
 
 }
