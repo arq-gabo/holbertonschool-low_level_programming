1edfec7b9cc2eb2e0e109ceaf9e01b0e9b974c8a
arq-gabo <jgabrielguerral@gmail.com>
2019-08-02 21:00:15 +0000

    [Fix]add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,50 +1,28 @@
 #include "lists.h"
-
 /**
- *_strlen - entry point
- *@s: a pointer to a word
- * Description: print tthe length of a word
- * Return: always 0
+ * add_node - adds a new node at the beginning of a list_t.
+ * @head: a pointer
+ * @str: a constant string
+ * Return: list.
  */
-
-int _strlen(char *s)
-{
-	int len;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-
-/**
- * add_node -  adds a new node at the beginning of a  list.
- *@head: is the beginning of the list.
- *@str: contain the string.
- *
- * Return: The numbers of nodes.
- */
-
 list_t *add_node(list_t **head, const char *str)
 {
 
-	list_t  *new_node;
+	list_t *new;
+	size_t length = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = _strlen(new_node->str);
-	new_node->next = *head;
-	*head = new_node;
+	new->str = strdup(str);
 
-	return (new_node);
+	while (str[length])
+		length++;
+	new->len = length;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+
 }
