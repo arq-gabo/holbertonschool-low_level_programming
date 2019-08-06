42d93bccc3323588e6e23aa8fda8cf94954a6ebf
arq-gabo <jgabrielguerral@gmail.com>
2019-08-06 19:03:07 +0000

    [FIX]advanced_fibonacci1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+
+	long int a = 1, b = 2, c, count;
+
+	printf("%li, %li, ", a, b);
+
+	for (count = 0 ; count <= 50 ; count++)
+	{
+		if (count != 50)
+		{
+			c = a + b;
+			printf("%li, ", c);
+			a = b;
+			b = c;
+		}
+		else
+		{
+			printf("%li", c);
+		}
+			
+	}
+	printf("\n");
+	return (0);
+}
