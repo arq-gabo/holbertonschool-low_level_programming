f7f46d053bfc000abd43cbd517435742be15842c
arq-gabo <jgabrielguerral@gmail.com>
2019-08-06 02:34:22 +0000

    [FEATURE]Singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint -  adds a new node at the beginning of a  list.
+ *@head: is the beginning of the list.
+ *@n: contain the integer.
+ *
+ * Return: The numbers of nodes.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
