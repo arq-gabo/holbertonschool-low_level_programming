a81984521e9c5d44617c0a20c7ee6a1412b6a936
arq-gabo <jgabrielguerral@gmail.com>
2019-08-07 02:42:22 +0000

    [FIX]Task_5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ *
+ * @head: pointer list
+ */
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t **current = head;
+	listint_t *old;
+
+	if (head == NULL)
+		return;
+
+	if (*current != NULL)
+	{
+		old = *current;
+		*current = (*current)->next;
+		free(old);
+	}
+	*head = NULL;
+}
