6c85781bbc204c64fe37779564aaed784f632a9a
arq-gabo <jgabrielguerral@gmail.com>
2019-08-09 06:03:44 +0000

    [Fix]Tasks_5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - check the number of bits .
+ * @n: number 1
+ * @m: number 2
+ * Return: number of bit to flip.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+		unsigned long int number;
+		unsigned long int eye = 1;
+		int count = 0;
+
+		eye <<= 63;
+
+		number = (n ^ m);
+
+		for (; eye > 0; eye >>= 1)
+		{
+			if (eye & number)
+			{
+				count++;
+			}
+		}
+
+		return (count);
+}
