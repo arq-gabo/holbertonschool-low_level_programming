107144feebf31c1219651df4871176c21169a0c6
arq-gabo <jgabrielguerral@gmail.com>
2019-08-09 03:56:31 +0000

    Correction_Betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,16 @@
  * Return: the converted number or 0
  */
 
-
 unsigned int binary_to_uint(const char *b)
 {
+
 	unsigned int sum;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
+
 	for (sum = 0; *b; b++)
 	{
 		if (*b == '1')
@@ -31,5 +32,7 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		}
 	}
+
 	return (sum);
+
 }
