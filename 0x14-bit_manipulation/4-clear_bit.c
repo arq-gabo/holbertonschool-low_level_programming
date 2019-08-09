4d53cdbd502039ca2d18cd4682564f7bc49eb539
arq-gabo <jgabrielguerral@gmail.com>
2019-08-09 05:38:48 +0000

    [FIX]Taks_4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * clear_bit - check the code for Holberton School students.
+ * @n: number
+ * @index: index to set 0.
+ * Return: 1 or -1.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int *pointer;
+	unsigned int ind = 1;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	pointer = n;
+	ind <<= index;
+
+	if (index == 0)
+	{
+
+		if (*pointer % 2 != 0)
+		{
+			*pointer -= ind;
+			return (1);
+		}
+
+		else
+			return (1);
+	}
+
+	if (*pointer >= ind && (*pointer & ind))
+		*pointer -= ind;
+
+	return (1);
+}
