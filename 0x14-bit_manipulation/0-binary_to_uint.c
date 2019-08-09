7ca590f96b898a13ec98e9d6ad03663b580cbb9c
arq-gabo <jgabrielguerral@gmail.com>
2019-08-09 03:50:20 +0000

    Inicial_File_Bit_Manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: string
+ * Return: the converted number or 0
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (sum = 0; *b; b++)
+	{
+		if (*b == '1')
+		{
+			sum = (sum << 1) | 1;
+		}
+		else if (*b == '0')
+		{
+			sum = sum << 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (sum);
+}
