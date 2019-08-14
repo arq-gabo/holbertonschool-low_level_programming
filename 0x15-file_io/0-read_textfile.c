c76c09fc4befe177d5ae3f5a4b35b83746d34f42
arq-gabo <jgabrielguerral@gmail.com>
2019-08-14 01:04:43 +0000

    FIX_Correction_Betty_Task_0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,6 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
 	int a1 = 0, lenght = 0, wri = 0;
 	char *buff;
 
@@ -17,38 +16,33 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	a1 = open(filename, O_RDONLY);
 	if (a1 == -1)
 	{
-		return(0);
+		return (0);
 	}
-
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 	{
 		close(a1);
 		free(buff);
-		return(0);
+		return (0);
 	}
-
 	lenght = read(a1, buff, letters);
 	if (lenght == -1)
 	{
 		close(a1);
 		free(buff);
-		return(0);
+		return (0);
 	}
-
 	wri = write(STDOUT_FILENO, buff, lenght);
 	if (wri == -1)
 	{
 		close(a1);
 		free(buff);
-		return(0);
+		return (0);
 	}
-
 	close(a1);
 	free(buff);
-	return(lenght);
+	return (lenght);
 }
