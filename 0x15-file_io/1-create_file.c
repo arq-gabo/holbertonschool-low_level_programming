e56aaedaa463a4a3513937b3bcfefbfe85d6d725
arq-gabo <jgabrielguerral@gmail.com>
2019-08-14 03:42:21 +0000

    FIX_Taks_1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,31 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int a1, b;
+	int fl, i = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 
-	a1 = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fl = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (text_content == NULL)
-	{
 		return (1);
-	}
 
-	if (a1 == -1)
+	if (fl == -1)
 	{
 		return (-1);
 	}
 
-	while (text_content[b])
-	{
-		b++;
-	}
-
-	write(a1, text_content, b);
-	close(a1);
+	while (text_content[i])
+		i++;
+	write(fl, text_content, i);
+	close(fl);
 	return (1);
 }
