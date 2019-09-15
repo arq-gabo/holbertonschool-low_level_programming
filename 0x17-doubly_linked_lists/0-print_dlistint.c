e6c5ceb7456f446d04fb6dfaad75320327c983f8
arq-gabo <jgabrielguerral@gmail.com>
2019-09-15 23:22:11 +0000

    [FEATURE]Correction_task_0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - print doubly linked list
+ * @h: Head List
+ * Return: Number of items of the linked list
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
