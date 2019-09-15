b3fcc885849af91a63627db0225b1deb13b249fc
arq-gabo <jgabrielguerral@gmail.com>
2019-09-15 23:56:19 +0000

    [FEATURE]Description_Task_2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * add_dnodeint - Add a node at the beginning of the double linked list
+ * @n : value to add to the list
+ * @head : head of the linked list
+ * Return: pointer to the element added
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
