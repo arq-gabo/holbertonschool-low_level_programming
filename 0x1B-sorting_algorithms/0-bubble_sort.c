fb7cec26527a9735d857e332e32c1b9fdd860b5e
arq-gabo <jgabrielguerral@gmail.com>
2019-10-28 17:46:56 +0000

    [FEATURE]Correction_task_00

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to classify an array of integers in ascending order
+ * @array: integers to be ordered
+ * @size: size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
+	size_t i, n_size = size;
+	int tmp, sort;
+
+	while (n_size)
+	{
+		sort = 1;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				sort = 0;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		if (sort == 1)
+		{
+			break;
+		}
+		n_size--;
+	}
+}
