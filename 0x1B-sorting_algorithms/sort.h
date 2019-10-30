efb8d73841e52bf6a48fc92b396535451afb839f
arq-gabo <jgabrielguerral@gmail.com>
2019-10-30 02:19:10 +0000

    [FEATURE]Upload_sort_file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRUE 1
+#define FALSE 0
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,5 +28,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
