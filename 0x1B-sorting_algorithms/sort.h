a123b4344659283c019002704c1b3af6cb58a3b5
arq-gabo <jgabrielguerral@gmail.com>
2019-10-30 23:15:22 +0000

    [FEATURE]Update_sort_file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -26,8 +26,19 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+
 void insertion_sort_list(listint_t **list);
+void extractor_node(listint_t *node);
+void insertion_head(listint_t **list, listint_t *new_head);
+void swap_adj(listint_t **head, listint_t *before, listint_t *after);
+void insertion_node(listint_t *target_node, listint_t *swap_node);
+
+
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
+int partition(int *array, size_t size, int start, int end);
+void qs_recurse(int *array, size_t size, int start, int end);
+
 
 #endif
