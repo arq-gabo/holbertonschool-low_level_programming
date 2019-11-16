cda76d92bb5ef4c733979737d2e14d214b70d5c7
arq-gabo <jgabrielguerral@gmail.com>
2019-11-16 06:05:09 +0000

    [FIX]Task_05

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,13 +9,13 @@ def island_perimeter(grid):
     for line in range(len(grid)):
         for col in range(len(grid[line])):
             if grid[line][col] is 1:
-                if grid[line][col - 1] is 0 and col >= 0:
+                if grid[line][col - 1] is 0 or col is 0:
                     i += 1
-                if grid[line][col + 1] is 0 and col < len(grid):
+                if grid[line][col + 1] is 0 or col is len(grid) - 1:
                     i += 1
-                if grid[line - 1][col] is 0 and line >= 0:
+                if grid[line - 1][col] is 0 or line is 0:
                     i += 1
-                if grid[line + 1][col] is 0 and line < len(grid[line]):
+                if grid[line + 1][col] is 0 or line is len(grid[line]) - 1:
                     i += 1
                 else:
                     pass
