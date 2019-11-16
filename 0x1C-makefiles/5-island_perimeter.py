e13775801a85982570bf0dd46a231ee74773ecca
arq-gabo <jgabrielguerral@gmail.com>
2019-11-16 06:59:52 +0000

    [FIX]Correction_conditions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+Module for find the perimeter
+"""
+
 
 def island_perimeter(grid):
     """Method for calculate the perimeter of the island"""
@@ -9,12 +13,12 @@ def island_perimeter(grid):
     for line in range(len(grid)):
         for col in range(len(grid[line])):
             if grid[line][col] is 1:
-                if grid[line][col - 1] is 0 or col is 0:
+                if col is 0 or grid[line][col - 1] is 0:
                     i += 1
-                if grid[line][col + 1] is 0 or col is len(grid) - 1:
+                if col is len(grid[line]) - 1 or grid[line][col + 1] is 0:
                     i += 1
-                if grid[line - 1][col] is 0 or line is 0:
+                if line is 0 or grid[line - 1][col] is 0:
                     i += 1
-                if grid[line + 1][col] is 0 or line is len(grid[line]) - 1:
+                if line is len(grid) - 1 or grid[line + 1][col] is 0:
                     i += 1
     return i
