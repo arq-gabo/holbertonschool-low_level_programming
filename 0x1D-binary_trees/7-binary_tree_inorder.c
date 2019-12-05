4062090be880673f1830e81a4838ea68c847b60b
arq-gabo <jgabrielguerral>
2019-12-05 02:31:18 +0000

    [FEATURE]Task_07

---
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - function with transverse using a path in order
+ * @tree: node of tree transverse
+ * @func: pointer to function to execute pre-order
+ * Return: if tree or func is null, return empty
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+
+}
