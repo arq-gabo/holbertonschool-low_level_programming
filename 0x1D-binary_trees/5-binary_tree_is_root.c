22c873266a188969a8e3a573e99801cc96e6129c
arq-gabo <jgabrielguerral>
2019-12-05 01:12:10 +0000

    [FEATURE]Task_05

---
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - funtion for verify if a node is a root
+ * @node: node for test
+ * Return: if node is root return 1, else return 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+
+	if (node == NULL)
+	{
+		return (0);
+	}
+	
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
