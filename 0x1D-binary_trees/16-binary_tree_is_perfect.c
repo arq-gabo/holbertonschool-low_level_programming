15d416c74a30990202fbd178f21d7cd8868c8988
arq-gabo <jgabrielguerral>
2019-12-06 19:03:06 +0000

    [FIX]Correction_betty_task_16

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -9,30 +9,29 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        if (tree == NULL)
-        {
-                return (0);
-        }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-        if (tree->left == NULL && tree->right == NULL)
-        {
-                return (0);
-        }
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
 
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-        if (left_height > right_height)
-        {
-                return (left_height + 1);
-        }
-        else
-        {
-                return (right_height + 1);
-        }
-
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
 }
 
 /**
@@ -44,23 +43,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 
-        size_t left_size = 0, right_size = 0;
+	size_t left_size = 0, right_size = 0;
 
-        if (tree == NULL)
-        {
-                return (0);
-        }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-        if (tree->left == NULL && tree->right == NULL)
-        {
-                return (1);
-        }
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 
-        left_size = binary_tree_size((const binary_tree_t *)(tree->left));
-        right_size = binary_tree_size((const binary_tree_t *)(tree->right));
+	left_size = binary_tree_size((const binary_tree_t *)(tree->left));
+	right_size = binary_tree_size((const binary_tree_t *)(tree->right));
 
-        return (left_size + 1 + right_size);
-
+	return (left_size + 1 + right_size);
 }
 
 /**
@@ -87,7 +85,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		{
 			perfect *= 2;
 		}
-		perfect = (perfect * 2) -1;
+		perfect = (perfect * 2) - 1;
 	}
 
 	if (perfect == (int)binary_tree_size(tree))
